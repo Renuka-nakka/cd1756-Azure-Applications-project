f6f170ba63fff61d25c156e7e6deb443a30a5ca5
Renuka-nakka <nakkarenuka456@gmail.com>
2026-03-12 16:34:07 +0530

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -1,64 +1,14 @@
-"""
-Routes and views for the flask application.
-"""
-
-from datetime import datetime
+import uuid
 from flask import render_template, flash, redirect, request, session, url_for
 from werkzeug.urls import url_parse
+from flask_login import current_user, login_user, logout_user, login_required
 from config import Config
 from FlaskWebProject import app, db
 from FlaskWebProject.forms import LoginForm, PostForm
-from flask_login import current_user, login_user, logout_user, login_required
 from FlaskWebProject.models import User, Post
 import msal
-import uuid
 
-imageSourceUrl = 'https://' + app.config['BLOB_ACCOUNT'] + '.blob.core.windows.net/' + app.config['BLOB_CONTAINER'] + '/'
-
-
-@app.route('/')
-@app.route('/home')
-@login_required
-def home():
-    user = User.query.filter_by(username=current_user.username).first_or_404()
-    posts = Post.query.all()
-    return render_template(
-        'index.html',
-        title='Home Page',
-        posts=posts
-    )
-
-
-@app.route('/new_post', methods=['GET', 'POST'])
-@login_required
-def new_post():
-    form = PostForm(request.form)
-    if form.validate_on_submit():
-        post = Post()
-        post.save_changes(form, request.files['image_path'], current_user.id, new=True)
-        return redirect(url_for('home'))
-    return render_template(
-        'post.html',
-        title='Create Post',
-        imageSource=imageSourceUrl,
-        form=form
-    )
-
-
-@app.route('/post/<int:id>', methods=['GET', 'POST'])
-@login_required
-def post(id):
-    post = Post.query.get(int(id))
-    form = PostForm(formdata=request.form, obj=post)
-    if form.validate_on_submit():
-        post.save_changes(form, request.files['image_path'], current_user.id)
-        return redirect(url_for('home'))
-    return render_template(
-        'post.html',
-        title='Edit Post',
-        imageSource=imageSourceUrl,
-        form=form
-    )
+imageSourceUrl = f'https://{app.config["BLOB_ACCOUNT"]}.blob.core.windows.net/{app.config["BLOB_CONTAINER"]}/'
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -70,22 +20,22 @@ def login():
 
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
 
         login_user(user, remember=form.remember_me.data)
-
         next_page = request.args.get('next')
-
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
-
         return redirect(next_page)
 
+    # Microsoft login
     session["state"] = str(uuid.uuid4())
-    auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
+    auth_url = _build_auth_url(
+        scopes=Config.SCOPE,
+        state=session["state"]
+    )
 
     return render_template(
         'login.html',
@@ -97,16 +47,17 @@ def login():
 
 @app.route(Config.REDIRECT_PATH)
 def authorized():
-
-    if request.args.get('state') != session.get("state"):
+    # Verify state
+    if request.args.get("state") != session.get("state"):
         return redirect(url_for("home"))
 
+    # Check for error
     if "error" in request.args:
         return render_template("auth_error.html", result=request.args)
 
-    if request.args.get('code'):
+    # Acquire token
+    if "code" in request.args:
         cache = _load_cache()
-
         result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
             request.args['code'],
             scopes=Config.SCOPE,
@@ -118,60 +69,46 @@ def authorized():
 
         session["user"] = result.get("id_token_claims")
 
-        user = User.query.filter_by(username="admin").first()
-        login_user(user)
+        # Use the Azure AD username or fallback to admin
+        username = session["user"].get("preferred_username", "admin")
+        user = User.query.filter_by(username=username).first()
+        if not user:
+            # Optionally create the user in your DB
+            user = User(username=username)
+            user.set_password(uuid.uuid4().hex[:10])  # temporary random password
+            db.session.add(user)
+            db.session.commit()
 
+        login_user(user)
         _save_cache(cache)
 
     return redirect(url_for('home'))
 
 
-@app.route('/logout')
-def logout():
-
-    logout_user()
-
-    if session.get("user"):
-        session.clear()
-
-        return redirect(
-            Config.AUTHORITY + "/oauth2/v2.0/logout" +
-            "?post_logout_redirect_uri=" + url_for("login", _external=True)
-        )
-
-    return redirect(url_for('login'))
-
-
 def _load_cache():
     cache = msal.SerializableTokenCache()
-
     if session.get("token_cache"):
         cache.deserialize(session["token_cache"])
-
     return cache
 
 
 def _save_cache(cache):
-
     if cache.has_state_changed:
         session["token_cache"] = cache.serialize()
 
 
 def _build_msal_app(cache=None, authority=None):
-
+    # Use tenant-specific authority
     return msal.ConfidentialClientApplication(
-        app.config["CLIENT_ID"],
-        authority=authority or Config.AUTHORITY,
+        client_id=app.config["CLIENT_ID"],
         client_credential=app.config["CLIENT_SECRET"],
+        authority=authority or Config.AUTHORITY,
         token_cache=cache
     )
 
 
 def _build_auth_url(authority=None, scopes=None, state=None):
-
-    return _build_msal_app(
-        authority=authority
-    ).get_authorization_request_url(
+    return _build_msal_app(authority=authority).get_authorization_request_url(
         scopes or [],
         state=state,
         redirect_uri=url_for("authorized", _external=True)
